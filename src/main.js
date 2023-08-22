72c16a02601e3d1d669d860bf40a57ed03dc197a
Wolmer <contact@wolmer.me>
2023-08-22 18:24:30 +0530

    boxicons, static => package

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { MotionPlugin } from '@vueuse/motion'
+import 'boxicons/css/boxicons.min.css'
+
 
 
 import './assets/main.css'
